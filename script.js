cda5388334a706731aed61d16ef8b8c0e2e4b38c
Peeeaaayus <91822564+Peeeaaayus@users.noreply.github.com>
2021-10-09 13:15:01 +0300

    Update script.js
    little fix

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         YouTube Cookies Popup Blocker
 // @namespace    https://github.com/Peeeaaayus
-// @version      0.1
+// @version      1.0
 // @description  Blocks the YouTube cookies popup.
 // @author       PIjus
 // @match        https://*.youtube.com/*
